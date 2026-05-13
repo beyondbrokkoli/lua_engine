eb39cdee07fc69f714fd0776fc9d3bfd66da7f74
beyondbrokkoli <halim.zag@gmx.de>
2026-05-13 17:58:30 +0200

    m

--- vmath.lua
+++ vmath.lua
@@ -19,16 +19,16 @@ local temp_u   = ffi.new("vec4_t")
 local temp_r   = ffi.new("vec4_t")
 local temp_mat = ffi.new("mat4_t")
 
-function vmath.lookAt(eye, center, up, out_mat)
+function vmath.lookAt(eye_x, eye_y, eye_z, center_x, center_y, center_z, out_mat)
     -- Forward Vector
-    temp_f.x = center.x - eye.x
-    temp_f.y = center.y - eye.y
-    temp_f.z = center.z - eye.z
+    temp_f.x = center_x - eye_x
+    temp_f.y = center_y - eye_y
+    temp_f.z = center_z - eye_z
     local f_inv = 1.0 / math.sqrt(temp_f.x^2 + temp_f.y^2 + temp_f.z^2)
     temp_f.x = temp_f.x * f_inv; temp_f.y = temp_f.y * f_inv; temp_f.z = temp_f.z * f_inv
 
-    -- Gimbal Lock Prevention (Imported from slow version)
-    local up_x = up.x; local up_y = up.y; local up_z = up.z
+    -- Gimbal Lock Prevention (Internal Up Vector)
+    local up_x = 0.0; local up_y = 1.0; local up_z = 0.0
     if math.abs(temp_f.x) < 0.001 and math.abs(temp_f.z) < 0.001 then
         if temp_f.y > 0 then up_z = -1.0 else up_z = 1.0 end
         up_y = 0.0; up_x = 0.0
@@ -47,9 +47,9 @@ function vmath.lookAt(eye, center, up, out_mat)
     temp_u.z = temp_f.x * temp_r.y - temp_f.y * temp_r.x
 
     -- Direct contiguous memory write
-    out_mat.m[0] = temp_r.x;  out_mat.m[4] = temp_r.y;  out_mat.m[8]  = temp_r.z;  out_mat.m[12] = -(temp_r.x*eye.x + temp_r.y*eye.y + temp_r.z*eye.z)
-    out_mat.m[1] = temp_u.x;  out_mat.m[5] = temp_u.y;  out_mat.m[9]  = temp_u.z;  out_mat.m[13] = -(temp_u.x*eye.x + temp_u.y*eye.y + temp_u.z*eye.z)
-    out_mat.m[2] = -temp_f.x; out_mat.m[6] = -temp_f.y; out_mat.m[10] = -temp_f.z; out_mat.m[14] = (temp_f.x*eye.x + temp_f.y*eye.y + temp_f.z*eye.z)
+    out_mat.m[0] = temp_r.x;  out_mat.m[4] = temp_r.y;  out_mat.m[8]  = temp_r.z;  out_mat.m[12] = -(temp_r.x*eye_x + temp_r.y*eye_y + temp_r.z*eye_z)
+    out_mat.m[1] = temp_u.x;  out_mat.m[5] = temp_u.y;  out_mat.m[9]  = temp_u.z;  out_mat.m[13] = -(temp_u.x*eye_x + temp_u.y*eye_y + temp_u.z*eye_z)
+    out_mat.m[2] = -temp_f.x; out_mat.m[6] = -temp_f.y; out_mat.m[10] = -temp_f.z; out_mat.m[14] = (temp_f.x*eye_x + temp_f.y*eye_y + temp_f.z*eye_z)
     out_mat.m[3] = 0.0;       out_mat.m[7] = 0.0;       out_mat.m[11] = 0.0;       out_mat.m[15] = 1.0
 end
 
